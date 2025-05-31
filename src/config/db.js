062859996702b84633b125d31f4ec4df46c205f9
erfan <sltanyh1384@gmail.com>
2025-05-31 17:04:28 +0330

    create project structure and folders

---
+++ src/config/db.js
@@ -0,0 +1,20 @@
+import pkg from "pg";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const { Pool } = pkg;
+
+const pool = new Pool({
+  user: process.env.DB_USER,
+  host: process.env.DB_HOST,
+  database: process.env.DB_DATABASE,
+  password: process.env.DB_PASSWORD,
+  port: process.env.DB_PORT,
+});
+
+pool.on("connect", () => {
+  console.log("Connection with database is OK!");
+});
+
+export default pool;
---
+++ src/index.js
@@ -0,0 +1,29 @@
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+import pool from "./config/db.js";
+import userroutes from "./routes/userroutes.js";
+import errorHandling from "./middlewares/errorHandler.js";
+
+dotenv.config();
+
+const app = express();
+const port = process.env.PORT || 3001;
+
+// middleware
+app.use(express.json());
+app.use(cors());
+
+// routes  //api
+app.use("/api", userroutes);
+// const result = await pool.query("select * from users");
+// console.log(result.rows);
+
+// error handling middleware
+app.use(errorHandling);
+
+// server running
+
+app.listen(port, () => {
+  console.log(`server is running on http://localhost:${port}`);
+});
