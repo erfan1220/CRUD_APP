e39512f97a5fff67dc9452f12a97e3d5d9621277
erfan <sltanyh1384@gmail.com>
2025-06-03 16:40:27 +0330

    implement sending email for user login page

--- src/controller/userController.js
+++ src/controller/userController.js
@@ -1,5 +1,11 @@
 import * as userService from "../models/userModel.js";
 
+import { Resend } from 'resend';
+
+const resend = new Resend('re_Xkpj4iiM_GdeTy9p6XqwEpXKtGmLYme5Z');
+let verificationCodes = {};
+
+
 const handleResponse = (res, status, message, data = null) => {
   res.status(status).json({
     status,
@@ -11,15 +17,29 @@ const handleResponse = (res, status, message, data = null) => {
 
 export const login = async (req, res, next) => {
   try {
-    const { user_number, user_password } = req.body;
-    const user = await userService.login(user_number);
+    // console.log(req.body);
+    const { user_email } = req.body;
+    const user = await userService.login(user_email);
+
     if (!user) {
       handleResponse(res, 404, "user not found")
+    } else {
+      const code = Math.floor(100000 + Math.random() * 900000);
+      verificationCodes[user_email] = {
+        code,
+        expires: Date.now() + 5 * 60 * 1000,
+      };
+      await resend.emails.send({
+        from: 'onboarding@resend.dev',
+        to: user_email,
+        subject: 'Your verification code',
+        html: `<h3>Hello Dear user,</h3></br> <p>Your verification code: <strong>${code}</strong></p>`
+      });
+      handleResponse(res, 200, "Code successfully sent")
     }
-    if (user.password != user_password) {
-      handleResponse(res, 401, "Invalid password")
-    }
-    handleResponse(res, 200, "successfull")
+    // if (user.password != user_password) {
+    //   handleResponse(res, 401, "Invalid password")
+    // }
   } catch (error) {
     next(error)
   }
--- src/models/userModel.js
+++ src/models/userModel.js
@@ -1,8 +1,8 @@
 import pool from "../config/db.js";
 
 
-export const login = async (phonenumber) => {
-  const result = await pool.query("select userpass AS password from users where phonenumber = $1", [phonenumber]);
+export const login = async (email) => {
+  const result = await pool.query("select userpass AS password from users where email = $1", [email]);
   return result.rows[0];
 }
 
